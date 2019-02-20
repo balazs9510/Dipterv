06549e45a1db3e89c1a1d3791093e11aae9a3764
Berta Balázs <95berta.balazs@gmail.com>
2019-02-20 13:08:42 +0100

    Initial commit.

---
+++ DAL/Entities/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class Booking
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public Guid LayoutPositionId { get; set; }
+        [Required]
+        public Guid ScheduleId { get; set; }
+        [Required]
+        public Guid UserId { get; set; }
+        // TODO nav propertyk
+    }
+}
